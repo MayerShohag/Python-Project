05e3495dd77663ee92aaeec6ac8bd1f9a29b26fe
SHOHAG KUMAR <118052454+MayerShohag@users.noreply.github.com>
2023-09-17 12:11:15 +0600

    Meal Management.py

--- Mill Management.py
+++ Mill Management.py
@@ -1,19 +1,19 @@
-total_money = float(input("Enter Total Money : "))
-total_mill = int(input("Enter Total Mill : "))
-total_mill_rate = float(total_money/total_mill)
+total_money = float(input("Enter Total Money : ")) # Manager total money
+total_meal = int(input("Enter Total Meal : ")) # Month total meal
+total_meal_rate = float(total_money/total_meal) # total money / total meal
 
 try:
-    for i in range(1, 25, 1):
-        name = input("Enter Name : ")
+    for i in range(1, 25, 1): 
+        name = input("Enter Name : ") 
         money = int(input("Enter Money : "))
-        mill = int(input("Enter Mill : "))
-        cost = (mill*total_mill_rate)
-        mp = int(money-cost)
+        meal = int(input("Enter Meal : "))
+        cost = (meal*total_meal_rate) # meal * total meal rate
+        mp = int(money-cost) # money-cost
         print("________________________")
         print("Name is : ", name)
         print("Given by the Manager : ", money)
-        print("Mill rate is : ", "%.2f" % total_mill_rate)
-        print("Mill ate : ", mill)
+        print("Meal rate is : ", "%.2f" % total_meal_rate)
+        print("Meal ate : ", meal)
         print("Cost is : ", "%.2f" % cost)
         print("Manager will give/receive : ", mp)
         print("________________________")
