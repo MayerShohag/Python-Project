d820fd6e5851d2698f29b865b255ddd83ad8956c
SHOHAG KUMAR <118052454+MayerShohag@users.noreply.github.com>
2023-09-17 12:02:08 +0600

    AutoComment.py

--- AutoComment.py
+++ AutoComment.py
@@ -1,17 +1,17 @@
-import pyautogui
-import time
+import pyautogui # import pyautogui
+import time # import time
 
-comment = ["Nice", "Good", "Not Bad", "Excellent", "Nice Caption", "Good Pose", "Wish You Good Luck", "Best of Luck"]
+comment = ["Nice", "Good", "Not Bad", "Excellent", "Nice Caption", "Good Pose", "Wish You Good Luck", "Best of Luck"] # your commment
 
 
 
-time.sleep(3)
+time.sleep(3) #program run time
 try:
 
-    for i in range(1, 20):
-        pyautogui.typewrite(comment[i%8])
-        pyautogui.typewrite("\n")
-        time.sleep(2)
-        pyautogui.press("Enter")
+    for i in range(1, 20): # loop is running 20 times
+        pyautogui.typewrite(comment[i%8]) # loop variable % comment index
+        pyautogui.typewrite("\n") # new line
+        time.sleep(2) # comment sent after 2 second
+        pyautogui.press("Enter") # automatically pressed enter button loop running time
 except:
-    print("Loop is over....!")
+    print("Loop is over....!") 
